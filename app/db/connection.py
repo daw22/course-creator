957f86b64853f9832427818a4d1449485257a1ab
daw22 <dawitxxd@gmail.com>
2025-11-03 15:41:29 +0300

    mongodb certify

--- app/db/connection.py
+++ app/db/connection.py
@@ -2,9 +2,9 @@ from pymongo import MongoClient
 from langgraph.checkpoint.mongodb import MongoDBSaver
 from dotenv import load_dotenv
 import os
-
+import certifi
 load_dotenv()
 
-client = MongoClient(os.getenv("DB_URI"))
+client = MongoClient(os.getenv("DB_URI"), tls=True, tlsCAFile=certifi.where())
 db = client["course_creator"]
 checkpointer = MongoDBSaver(db)
